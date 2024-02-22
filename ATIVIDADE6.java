2334c3a3d4cacf0406bbb00e5a23cff0c613596b
Anderson-Klein <160682630+Anderson-Klein@users.noreply.github.com>
2024-02-21 23:25:41 -0300

    Update ATIVIDADE6.java

--- ATIVIDADE6.java
+++ ATIVIDADE6.java
@@ -1,1 +1,16 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
 
+public class Main {
+    public static void main(String[] args) {
+       
+        int numeroProduto = 123; 
+        double precoProduto = 25; 
+        String descricaoProduto = "Fone de ouvido Bluetooth";
+    
+        System.out.println("Dados do Produto:");
+        System.out.println("Numero: " + numeroProduto);
+        System.out.println("Preco: " + precoProduto);
+        System.out.println("Descricao: " + descricaoProduto);
+    }
+}
