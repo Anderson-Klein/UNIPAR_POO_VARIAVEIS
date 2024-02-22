ecc083e9d3302454b781fcf38b6aac88f381361c
Anderson-Klein <160682630+Anderson-Klein@users.noreply.github.com>
2024-02-21 23:35:50 -0300

    Create ATIVIDADE7.java

---
+++ ATIVIDADE7.java
@@ -0,0 +1,18 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+public class ControleEstoque {
+    public static void main(String[] args) {
+     
+        String nomeProduto = "Fone de Ouvido";
+        double precoProduto = 29.99;
+        int quantidadeProduto = 10;
+        String marcaProduto = "true";
+        
+        System.out.println("Dados do Produto:");
+        System.out.println("Nome: " + nomeProduto);
+        System.out.println("Marca: " + marcaProduto);
+        System.out.println("Preco: " + precoProduto);
+        System.out.println("Quantidade em estoque: " + quantidadeProduto);
+    }
+}
